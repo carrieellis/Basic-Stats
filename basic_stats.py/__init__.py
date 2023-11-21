425643a6d44c07242138a437c512c07b29dce08e
carrieellis <carrieellis@uri.edu>
2023-11-21 17:34:59 -0500

    Update 1

---
+++ basic_stats.py/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Basic-Stats Package for Python
+    Version 1.0 Nov. 21, 2023
+    Author: Carrie Ellis, URI College of Engineering
+    Email: carrieellis@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .Ellis_BasicStats2 import *
+
+
+__all__ = ['sat_vp']
